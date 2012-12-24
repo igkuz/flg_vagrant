67774bcf839ccb54d25497fed8d9a7130f961e76
Igor Kuznetsov <igkuznetsov@gmail.com>
2012-12-25 00:27:53 +0400

    Added unuseful information to metadata about maintainer and usefull about dependencies

---
+++ my-cookbooks/flg/metadata.rb
@@ -0,0 +1,6 @@
+name                      "FLG"
+maintainer                "HPHMAC Inc."
+license                   "Private"
+description               "Installs and configures FLG project"
+
+depends "php"
